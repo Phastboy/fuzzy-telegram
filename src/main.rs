9ffd8b5436f367e243043f6de8608c8304407549
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-01-06 22:36:57 +0100

    using `mut`

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,10 @@ fn main() {
 
     let pi=3.141592;
     println!("{ } value to 3 decimal place is {pi:.*}", "pi", 3);
+
+    let mut temp=1;
+    println!("{ } initial value is {temp}", "temp");
+
+    temp=5;
+    println!("{ } final value is {temp}", "temp");
 }
