8f3bdf755f9218510f27db13632e9001391cd940
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-01-15 02:58:45 +0100

    using standard input output

--- src/area_of_rect.rs
+++ src/area_of_rect.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn calculate_area(length: f64, width: f64) -> f64 {
     println!("length: {}", length);
     println!("width: {}", width);
@@ -5,8 +7,32 @@ fn calculate_area(length: f64, width: f64) -> f64 {
 }
 
 fn main() {
-    let length: f64 = 5.0;
-    let width: f64 = 3.5;
+    let mut input = String::new();
+
+    // get length
+    println!("Enter the length of the rectangle: ");
+    io::stdin().read_line(&mut input).expect("failed ❌ to read input");
+    let length: f64 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid ❌ input for length . Please 🙏 enter a valid number.");
+            return;
+        }
+    };
+
+    input.clear();
+
+    // get width
+    println!("Enter the width of the rectangle: ");
+    io::stdin().read_line(&mut input).expect("failed ❌ to read input");
+    let width: f64 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid ❌ input for width. Please 🙏 enter a valid number.");
+            return;
+        }
+    };
+
     let area: f64 = calculate_area(length, width);
     println!("Area of the rectangle: {:.3}", area); // to 3 d.p
 }
