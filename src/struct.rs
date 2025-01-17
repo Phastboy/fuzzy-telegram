3ef98e971dae91b3c907a6dbdab23cfaa8f5a3a9
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-01-17 23:43:25 +0100

    handle input for struct

--- src/struct.rs
+++ src/struct.rs
@@ -1,31 +1,52 @@
+use std::io;
+
 #[derive(Debug)]
 struct Rectangle {
     length: f64,
     width: f64,
 }
 
-impl Rectangle {
-    // Method to calculate the area
+trait Shape {
+    fn area(&self)->f64;
+    fn perimeter(&self)->f64;
+}
+
+impl Shape for Rectangle {
     fn area(&self) -> f64 {
         self.length * self.width
     }
 
-    // Method to calculate the perimeter
     fn perimeter(&self) -> f64 {
         2.0 * (self.length + self.width)
     }
 }
 
 fn main() {
-    // Create an instance of Rectangle
-    let rect: Rectangle = Rectangle {
-        length: 5.0,
-        width: 3.0,
-    };
+    let length = get_input("What is the length of your rectangle?");
+    let width = get_input("What is the width of your rectangle?");
 
-    // Access fields and methods
+    // instance of Rectangle
+    let rect = Rectangle { length, width };
+
     println!("Length: {}", rect.length);
     println!("Width: {}", rect.width);
     println!("Area: {}", rect.area());
     println!("Perimeter: {}", rect.perimeter());
+}
+
+// Helper function to handle input
+fn get_input(prompt: &str) -> f64 {
+    loop {
+        let mut input = String::new();
+        println!("{}", prompt);
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed ❌ to read input.");
+
+        match input.trim().parse::<f64>() {
+            Ok(num) => return num,
+            Err(_) => println!("Invalid input ❌. Please enter a valid number."),
+        }
+    }
 }
