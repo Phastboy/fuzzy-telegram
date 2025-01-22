7afb6a25be3ca01b1302bb2109802189fdfe2e2e
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-01-22 19:12:33 +0100

    extraction with `unwrap()` and its bluds

--- src/option.rs
+++ src/option.rs
@@ -8,6 +8,10 @@ fn divide(numerator: f64, denominator: f64) -> Option<f64> {
 
 fn main() {
     let numerator = 7.4;
-    let denominator = 8.9;
-    println!("{} divided by {} is {:#?}", numerator, denominator, divide(numerator, denominator));
+    let denominator = 0.0;
+    let result=divide(numerator, denominator).unwrap_or_else(|| {
+        println!("Division by zero is undefined.");
+        0.0
+    });
+    println!("{} divided by {} is {}", numerator, denominator, result);
 }
